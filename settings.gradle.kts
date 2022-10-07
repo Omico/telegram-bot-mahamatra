46dc26c532de018e61c0bd5d6c79a16214de7e89
Omico <me@omico.icu>
2022-10-07 00:42:51 -0700

    Support load local Telegram Bot Api
    For testing purposes, the default logger in this library prints too much garbage information.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
+import java.util.Properties
+
 rootProject.name = "telegram-bot-mahamatra"
+
+val localProperties = Properties().apply {
+    val file = file("local.properties")
+    if (file.exists()) file.inputStream().use(::load)
+}
+
+val localTelegramBotApiDir: String? = localProperties.getProperty("telegram.bot.api.dir", null)
+
+if (localTelegramBotApiDir != null) includeBuild(localTelegramBotApiDir) {
+    dependencySubstitution {
+        substitute(module("eu.vendeli:telegram-bot")).using(project(":telegram-bot"))
+    }
+}
