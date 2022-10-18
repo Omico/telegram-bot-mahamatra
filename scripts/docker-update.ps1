851deecbdae7c27d79e5d85c459f6d462b413d6d
Omico <me@omico.icu>
2022-10-18 10:31:55 -0700

    scripts: Add docker-update.ps1

---
+++ scripts/docker-update.ps1
@@ -0,0 +1,24 @@
+function Test-Command($Command = $null) {
+    if ($null -eq $Command) {
+        return $false
+    }
+    $oldPreference = $ErrorActionPreference
+    $ErrorActionPreference = "stop"
+    try { if (Get-Command $Command) { return $true } }
+    catch { Write-Host "$Command does not exist"; return $false }
+    finally { $ErrorActionPreference = $oldPreference }
+}
+
+if (Test-Command("git")) {
+    git pull
+}
+else {
+    Write-Host "Git does not exist."
+}
+
+if (Test-Command("docker-compose")) {
+    docker-compose up -d --build --force-recreate
+}
+else {
+    Write-Host "Docker does not exist."
+}
