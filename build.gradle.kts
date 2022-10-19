5bc1cc38ee671304edf06cad847f9e7f5d20c84d
Omico <me@omico.icu>
2022-10-19 09:46:16 -0700

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,15 @@ application {
 }
 
 dependencies {
-    compileOnly("io.ktor:ktor-client-core:2.1.1")
-    implementation("eu.vendeli:telegram-bot:2.2.1")
+    val ktorVersion = "2.1.2"
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("eu.vendeli:telegram-bot:2.2.2") {
+        exclude(group = "io.ktor")
+    }
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     testImplementation(kotlin("test"))
 }
 
