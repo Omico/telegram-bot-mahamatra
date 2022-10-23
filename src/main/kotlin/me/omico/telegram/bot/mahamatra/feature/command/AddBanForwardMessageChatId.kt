9e78bb663c88a0181ab4d2d672d18f803acf994f
Omico <me@omico.icu>
2022-10-22 22:40:18 -0700

    ban: Handle result

--- src/main/kotlin/me/omico/telegram/bot/mahamatra/feature/command/AddBanForwardMessageChatId.kt
+++ src/main/kotlin/me/omico/telegram/bot/mahamatra/feature/command/AddBanForwardMessageChatId.kt
@@ -1,10 +1,13 @@
 package me.omico.telegram.bot.mahamatra.feature.command
 
 import eu.vendeli.tgbot.TelegramBot
+import eu.vendeli.tgbot.api.message
 import eu.vendeli.tgbot.types.Message
 import me.omico.telegram.bot.mahamatra.modifyConfiguration
 import me.omico.telegram.bot.utility.chatOwnerOrAdministratorOnly
 import me.omico.telegram.bot.utility.onCommand
+import me.omico.telegram.bot.utility.sendTimeLimited
+import kotlin.time.Duration.Companion.seconds
 
 private const val COMMAND_ADD_BAN_FORWARD_MESSAGE_CHAT_ID = "/add_ban_forward_message_chat_id"
 
@@ -12,16 +15,26 @@ context (TelegramBot)
     suspend fun Message.setupAddBanForwardMessageChatId() =
     onCommand(COMMAND_ADD_BAN_FORWARD_MESSAGE_CHAT_ID) { text ->
         chatOwnerOrAdministratorOnly {
-            val chatIds = text
-                .removePrefix(COMMAND_ADD_BAN_FORWARD_MESSAGE_CHAT_ID).trim()
-                .split(" ", ",")
-                .mapNotNull(String::toLongOrNull)
-            modifyConfiguration {
-                copy(
-                    banForwardMessage = banForwardMessage.copy(
-                        chatIds = banForwardMessage.chatIds + chatIds,
-                    ),
+            runCatching { addBanForwardMessageChatId(text) }
+                .fold(
+                    onSuccess = { "Added." },
+                    onFailure = { "Add failed: ${it.message ?: "Unknown error"}" },
                 )
-            }
+                .let(::message)
+                .sendTimeLimited(duration = 3.seconds, to = chat.id, via = this@TelegramBot)
         }
     }
+
+private suspend fun addBanForwardMessageChatId(text: String) {
+    val chatIds = text
+        .removePrefix(COMMAND_ADD_BAN_FORWARD_MESSAGE_CHAT_ID).trim()
+        .split(" ", ",")
+        .mapNotNull(String::toLongOrNull)
+    modifyConfiguration {
+        copy(
+            banForwardMessage = banForwardMessage.copy(
+                chatIds = banForwardMessage.chatIds + chatIds,
+            ),
+        )
+    }
+}
