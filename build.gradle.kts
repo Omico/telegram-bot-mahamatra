01bb3327374bfb4863031a907d555c62a0c2132b
Omico <me@omico.icu>
2024-03-23 06:17:23 -0700

    Update Kotlin to 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import java.util.Properties
 
 plugins {
     application
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "me.omico.telegram.bot.mahamatra"
